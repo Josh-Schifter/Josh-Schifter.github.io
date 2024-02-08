0c6d27ae31b888539fc0468de4de61b9d12fff2e
Josh Schifter <josh.schifter@bentley.com>
2024-02-08 11:45:13 -0500

    Missing some braces

--- script.js
+++ script.js
@@ -171,6 +171,9 @@ var sketchProc = function(processingInstance) {
         keys.splice(keys.indexOf(keyCode), 1);
         
     };
-    
+
+  }
+}
+
   var canvas = document.getElementById("canvas"); 
   var processingInstance = new Processing(canvas, sketchProc);
